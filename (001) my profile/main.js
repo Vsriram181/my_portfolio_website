5e10b57a8eed2772b2a9ea3adb45b060da82699e
VELIDI SRIRAM CHOWDARY <130280153+Vsriram181@users.noreply.github.com>
2023-10-02 00:32:13 +0530

    Add files via upload
    (000) file has total portfolio and each part of my total portfolio is again separately made in to  (001) to (004) files

---
+++ (001) my profile/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text', {
+    strings: ["Data Science", "Machine learning","Web Development"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+  });
